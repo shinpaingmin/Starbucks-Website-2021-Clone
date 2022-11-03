0ed56e49c605599f13afd448657cacf027265c74
Shin Paing Min <shinpaingmin@gmail.com>
2022-11-03 13:58:41 +0630

    finish

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const btn = document.getElementById('menu-btn');
+const mobile_menu = document.getElementById('menu')
+
 
 btn.addEventListener('click', () => {
     btn.classList.toggle('open')
+    mobile_menu.classList.toggle('open-menu')
+    document.body.classList.toggle('no-scroll')
 })
